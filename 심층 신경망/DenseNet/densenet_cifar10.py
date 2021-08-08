c6ef97933b9bccbe2cf8dbd1ea06c86660f01986
Bucket1572 <sout0811@naver.com>
2021-08-09 00:21:54 +0900

    DenseNet: Computing with GPU

--- 심층 신경망/DenseNet/densenet_cifar10.py
+++ 심층 신경망/DenseNet/densenet_cifar10.py
@@ -5,8 +5,17 @@ from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
 from tensorflow.keras.utils import to_categorical, plot_model
+import tensorflow as tf
 import numpy as np
 import os
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+        print(e)
 
 # Warning 끄기
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
