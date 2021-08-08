58aba1823a33b25dbcd3cf5ca71e140088044ad3
Bucket1572 <sout0811@naver.com>
2021-08-09 00:49:02 +0900

    Deep Model: 심층 신경망 검증 스크립트 작성

---
+++ 심층 신경망/심층 신경망 검증/load_and_evaluate_model.py
@@ -0,0 +1,29 @@
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+import os
+
+# 텐서플로의 GPU Warning 끄기
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# 로딩할 디렉토리
+dir = "../ResNet/saved_models/cifar10_resnet-v1_.010.h5"
+model = load_model(dir)
+
+# 데이터셋 로드
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Preprocessing
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# 설정
+batch_size = 32
+
+score = model.evaluate(
+    x_test, y_test, batch_size=batch_size
+)
+print(f"\nTest accuracy: {format(100.0 * score[1], '.1f')}%")
